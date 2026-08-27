a494ff68efc96518ee45ab535725e47f5a30c43a
QasimNaseer313 <qasim.naseer.314@email.com>
2026-08-27 17:58:49 +0500

    fix dj_database_url config

--- core/settings.py
+++ core/settings.py
@@ -165,8 +165,7 @@ SIMPLE_JWT = {
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.config(
-        default=DATABASE_URL,
+    DATABASES['default'] = dj_database_url.parse(
+        DATABASE_URL,
         conn_max_age=600,
-        ssl_require=True
     )
